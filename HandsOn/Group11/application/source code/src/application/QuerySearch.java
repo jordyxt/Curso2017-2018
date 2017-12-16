1ed7546dcaa48766b166329b84ec3c7368e6a676
jordyxt <31999881+jordyxt@users.noreply.github.com>
2017-12-16 13:37:56 +0100

    Add files via upload

--- HandsOn/Group11/application/source code/src/application/QuerySearch.java
+++ HandsOn/Group11/application/source code/src/application/QuerySearch.java
@@ -2,7 +2,16 @@ package application;
 
 import java.util.LinkedList;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.RDFNode;
+
 import javafx.scene.image.Image;
+import virtuoso.jena.driver.VirtGraph;
+import virtuoso.jena.driver.VirtuosoQueryExecution;
+import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
 
 public class QuerySearch {
 
@@ -13,9 +22,23 @@ public class QuerySearch {
 	}
 
 	public static LinkedList<Event> executeQuery(String query){
-		LinkedList<Event> results = new LinkedList<Event>();
+		LinkedList<Event> resultslist = new LinkedList<Event>();
 		//TODO
-		return results;
+		//La comprobacion se hizo en la connectToData
+		//por tanto no hacemos un try-catch
+		String url = "jdbc:virtuoso://localhost:1111";
+		VirtGraph set = new VirtGraph("http://localhost:8890/datosevento",url, "dba", "dba");	
+		Query sparql = QueryFactory.create(query);
+		VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create (sparql, set);
+		ResultSet results = vqe.execSelect();
+		while (results.hasNext()) {
+			QuerySolution result = results.nextSolution();
+		    RDFNode name = result.get("name");
+		    RDFNode description = result.get("description");
+		    RDFNode uriToLocation = result.get("location");
+		    resultslist.add(new Event(name.toString(), description.toString(), uriToLocation.toString())) ;
+		}
+		return resultslist;
 	}
 	
 	public static Image getMapImage(String locationURI){
